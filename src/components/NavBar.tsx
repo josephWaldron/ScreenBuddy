80313ace0c60b2e015c987b5495cfeb7871e5ecf
Joseph Waldron <waldrojo@kean.edu>
2023-04-29 15:43:52 -0400

    responsive?

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,17 +4,24 @@ import darkModeLogo from "../assets/ScreenBuddy-logos_white.png";
 import {
   Box,
   Button,
-  HStack,
+  Flex,
   Image,
-  Spacer,
   Text,
   useColorModeValue,
+  Spacer,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  useBreakpointValue,
 } from "@chakra-ui/react";
-import { MdPerson, MdLogin } from "react-icons/md";
+import { MdPerson, MdLogin, MdMenu } from "react-icons/md";
 
 const NavBar = () => {
   const logo = useColorModeValue(lightModeLogo, darkModeLogo);
   const { user, isLoaded } = useUser();
+  const isSmallerThanMd = useBreakpointValue({ base: true, sm: false });
 
   const handleProfile = () => {
     location.href = `/profile`;
@@ -22,50 +29,73 @@ const NavBar = () => {
   const handleSignIn = () => {
     location.href = "/sign-up";
   };
+
   if (!isLoaded)
     return (
-      <HStack padding={2} height={75}>
+      <Flex padding={2} height={75} alignItems="center">
         <Box width={50}> </Box>
         <Spacer />
         <a href="/">
-          <Text fontSize={"3xl"}>ScreenBuddy</Text>
+          <Text fontSize={"3xl"}>ScreenBuddy BETA</Text>
         </a>
         <Spacer />
         <Box width={100}></Box>
-      </HStack>
+      </Flex>
     );
 
   return (
-    <HStack padding={2} height={75}>
+    <Flex padding={2} height={75} alignItems="center">
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          aria-label="Options"
+          icon={<MdMenu />}
+          size="md"
+          variant="outline"
+        />
+        <MenuList>
+          <MenuItem as="a" href="/">
+            About
+          </MenuItem>
+          <MenuItem as="a" href="/">
+            Report Issue
+          </MenuItem>
+        </MenuList>
+      </Menu>
+
       <a href="/">
-        <Image src={logo} width={50} />
+        <Image src={logo} width={50} marginLeft={10} />
       </a>
       <Spacer />
-      <a href="/">
-        <Text fontSize={"3xl"}>ScreenBuddy</Text>
-      </a>
-      <Spacer />
-      {user && (
-        <Button
-          width={100}
-          rightIcon={<MdPerson />}
-          colorScheme="green"
-          onClick={handleProfile}
-        >
-          Profile
-        </Button>
+      {!isSmallerThanMd && (
+        <>
+          <a href="/">
+            <Text fontSize={"3xl"}>ScreenBuddy BETA</Text>
+          </a>
+          <Spacer />
+          {user && (
+            <Button
+              width={100}
+              rightIcon={<MdPerson />}
+              colorScheme="green"
+              onClick={handleProfile}
+            >
+              Profile
+            </Button>
+          )}
+          {!user && (
+            <Button
+              width={100}
+              rightIcon={<MdLogin />}
+              colorScheme="blue"
+              onClick={handleSignIn}
+            >
+              Sign In
+            </Button>
+          )}
+        </>
       )}
-      {!user && (
-        <Button
-          width={100}
-          rightIcon={<MdLogin />}
-          colorScheme="blue"
-          onClick={handleSignIn}
-        >
-          Sign In
-        </Button>
-      )}
-    </HStack>
+    </Flex>
   );
 };
 
